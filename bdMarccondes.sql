abe1464d5a813ae158a620fb6119730b15a0881c
vitor hugo tomé <133161885+vitinho13y7@users.noreply.github.com>
2026-05-14 17:19:35 -0300

    feat(bdMarcondes.sql)
    inclusão das procuderes

--- bdMarccondes.sql
+++ bdMarccondes.sql
@@ -85,3 +85,127 @@ CREATE TABLE Dependente_aluno(
     status ENUM('Ativo', 'Inativo', 'Removido') DEFAULT 'Ativo',
     FOREIGN KEY(id_responsavel) REFERENCES Cliente(id_cliente) ON DELETE CASCADE
 );
+
+-- Cadastrar Produto + Criar Estoque
+
+DELIMITER //
+
+CREATE PROCEDURE sp_cadastrar_produto (
+  IN p_nome VARCHAR(50),
+  IN p_valor DECIMAL(10,2),
+  IN p_categoria VARCHAR(50)
+)
+BEGIN
+  INSERT INTO Produto (nome_produto, valor_unitario, categoria)
+  VALUES (p_nome, p_valor, p_categoria);
+
+  INSERT INTO Estoque (id_produto, qtd)
+  VALUES (LAST_INSERT_ID(), 0);
+END //
+
+DELIMITER ;
+
+-- Atualizar Estoque (entrada ou saída)
+
+DELIMITER //
+
+CREATE PROCEDURE sp_atualizar_estoque (
+  IN p_id_produto INT,
+  IN p_qtd INT
+)
+BEGIN
+  UPDATE Estoque
+  SET qtd = qtd + p_qtd
+  WHERE id_produto = p_id_produto;
+END //
+
+DELIMITER ;
+
+-- Cadastrar Cliente
+
+DELIMITER //
+
+CREATE PROCEDURE sp_cadastrar_cliente (
+  IN p_nome VARCHAR(70),
+  IN p_telefone CHAR(11),
+  IN p_turma VARCHAR(20),
+  IN p_tipo ENUM('Aluno','Responsável','Professor','Funcionário')
+)
+BEGIN
+  INSERT INTO Cliente (nome, telefone_cliente, turma, tipo_cliente)
+  VALUES (p_nome, p_telefone, p_turma, p_tipo);
+END //
+
+DELIMITER ;
+
+--  Criar Venda
+
+DELIMITER //
+
+CREATE PROCEDURE sp_criar_venda (
+  IN p_id_cliente INT
+)
+BEGIN
+  INSERT INTO Venda (id_cliente)
+  VALUES (p_id_cliente);
+END //
+
+DELIMITER ;
+
+-- Adicionar Item na Venda (baixa estoque)
+
+DELIMITER //
+
+CREATE PROCEDURE sp_adicionar_item_venda (
+  IN p_id_venda INT,
+  IN p_id_produto INT,
+  IN p_qtd INT
+)
+BEGIN
+  DECLARE v_valor DECIMAL(10,2);
+
+  SELECT valor_unitario
+  INTO v_valor
+  FROM Produto
+  WHERE id_produto = p_id_produto;
+
+  INSERT INTO Itens_compra (id_venda, id_produto, qtd_compra, valor_unitario)
+  VALUES (p_id_venda, p_id_produto, p_qtd, v_valor);
+
+  UPDATE Estoque
+  SET qtd = qtd - p_qtd
+  WHERE id_produto = p_id_produto;
+END //
+
+DELIMITER ;
+
+-- Finalizar Venda
+
+DELIMITER //
+
+CREATE PROCEDURE sp_finalizar_venda (
+  IN p_id_venda INT
+)
+BEGIN
+  UPDATE Venda
+  SET status = 'Concluída'
+  WHERE id_venda = p_id_venda;
+END //
+
+DELIMITER ;
+
+-- Registrar Pagamento
+
+DELIMITER //
+
+CREATE PROCEDURE sp_registrar_pagamento (
+  IN p_id_venda INT,
+  IN p_id_formaPgto INT,
+  IN p_prestacao INT
+)
+BEGIN
+  INSERT INTO Pagamento (id_venda, id_formaPgto, prestacao)
+  VALUES (p_id_venda, p_id_formaPgto, p_prestacao);
+END //
+
+DELIMITER ;
